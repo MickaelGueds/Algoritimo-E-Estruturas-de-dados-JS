6d347ef0f562c7786848c316debc3e616dda14b0
MicksCoder <mickaelgoliveira@gmail.com>
2024-04-04 10:37:14 -0300

    Atualizações de algumas funções e melhorias

--- ListaListada/ListasDuplamenteLigadas.js
+++ ListaListada/ListasDuplamenteLigadas.js
@@ -5,41 +5,41 @@ import Node from "./models/linked-list-models"
 //lembre-se, o argumento do super serve para a incialização obrigatoria,o super em si ja chama todos os elementos
 // anteriores da classe
 class NodeDobrado extends Node {
-    constructor(element, next, prev){
+    constructor(element, next, prev) {
         super(element, next)
         this.prev = prev
     }
 }
-class ListaDobrada extends ListaLigada{
-    constructor(equalsFn = defaultEquals){
+class ListaDobrada extends ListaLigada {
+    constructor(equalsFn = defaultEquals) {
         super(equalsFn)
         this.tail = undefined
     }
-    insert(element,index){
+    insert(element, index) {
         //algo MUITO IMPORTANTE, lembre que aqui voce não cria o novo elemento, voce cria o ponteiro
         // e suas direções
-        if(index >= 0 && index <= this.count){
+        if (index >= 0 && index <= this.count) {
             const node = new ListaDobrada
             let current = this.head
-            if(index === 0){
-                if (this.head == null){
+            if (index === 0) {
+                if (this.head == null) {
                     //aqui caso esteja vazia a lista
                     //aqui voce inicializa com a primeiro ponteiro tanto para a cabeça quando para o final da fila
                     this.head = node
                     this.tail = node
-                }else{//lembre-se aqui voce ainda quer botar na posição 0, mas agora a lista não esta vazia
+                } else {//lembre-se aqui voce ainda quer botar na posição 0, mas agora a lista não esta vazia
                     //voce vai referenciar o proximo nó do novo elemento que ta colocando(que no caso vai ser o antigo elemento que ta no indice 0),para o elemento atual que é o de this.head, imagine que ta botando uma bola antes da bola que ja tem e voce precisa dizer pra essa nova bola para quem ela tem que apontar
                     //vai referenciar que o no anterior ao atual(no caso o antigo no de indice 0) vai ser apontado para o novo no que esta sendo colocado, apontando a bola que foi pra frente para a bola anterior, isso significa agora ques as duas bolas tem seus refenciais apontados 
                     node.next = this.head
                     current.prev = node
                     this.head = node
                 }
-            } else if(index === this.count){//aqui é a mesma loja de onteiros acima porem para o final
+            } else if (index === this.count) {//aqui é a mesma loja de onteiros acima porem para o final
                 current = this.tail
                 current.next = node
                 node.prev = current
                 this.tail = node
-            }else{
+            } else {
                 const previous = this.getElement(index - 1)
                 current = previous.next
                 node.next = current
@@ -50,7 +50,31 @@ class ListaDobrada extends ListaLigada{
             this.count++
             return true
         }
-        return false 
+        return false
     }
-    removeAt(index){}
+    removeAt(index) {
+        if (index >= 0 && index < this.count) {
+            let current = this.head
+            if (index === 0) {
+                this.head = current.next
+                if (this.count = 1) {
+                    this.tail = undefined
+                } else {
+                    this.head.prev = undefined
+                }
+            } else if (index === this.count) {
+                current = this.tail
+                this.tail = current.prev
+                this.tail.next = undefined
+            } else {
+                current = this.getElement(index)
+                const previous = current.prev
+                previous.next = current.next
+                current.next.prev = previous
+            }
+            this.count--
+            return current.element
+        }
+        return undefined
+    }
 }
